ee263f421b52a6356dcaff6cf20e66fcd737b3b3
Jordan <Jordanabid@gmail.com>
2023-11-22 13:12:06 +0100

    BOM + Pokemon + Tomcat

--- WEB/JavaScript/TP/TP10/Pokemon-Jordan/pikachu.js
+++ WEB/JavaScript/TP/TP10/Pokemon-Jordan/pikachu.js
@@ -4,6 +4,28 @@
 //4) Modifier la position de la div pikachu en fonction de la direction (+-30px par deplacement) et changer l'image
 //5) Verifier que pikachu ne sort pas de la div grass
 
+
+
+
+inputName.onkeyup=function(event)
+{
+  if(inputName.value!="")
+  {
+    btnStart.disabled=false;
+    if(event.key=="Enter")
+    {
+      lancerAventure();
+    }
+  }
+  else
+  {
+    btnStart.disabled=true;
+  }
+};
+
+
+btnStart.onclick=lancerAventure;
+
 var posX=0;
 var posY=0;
 var mouvement=30;
@@ -12,27 +34,52 @@ var direction="Down";
 imgPikachu.setAttribute("src","assets/img/"+pokemon+direction+".png");
 
 
+function lancerAventure()
+{
+  formStart.style.display="none";
+  grass.style.display="block";
+  pikachu.setAttribute("title",inputName.value);
+  document.body.onkeydown=deplacement;
+  themePokemon.play();
+
+  setTimeout(function(){themePokemon.pause();},3000);
+}
+
+
 
 function deplacement(event)
 {
   if(event.key=="ArrowDown" || event.key=="s" )
   {
-
+    if(posY<=630){
+        posY+=mouvement;
+    }
+    direction="Down";
   }
   else if(event.key=="ArrowRight" || event.key=="d" )
   {
-
+    if(posX<=630){
+        posX+=mouvement;
+    }
+    direction="Right";
   }
 
   else if(event.key=="ArrowLeft" || event.key=="q")
   {
-
+    if(posX>=30){
+        posX-=mouvement;
+    }
+    direction="Left";
   }
 
   else if(event.key=="ArrowUp" || event.key=="z")
   {
-
+    if(posY>=30){
+        posY-=mouvement;
+    }
+    direction="Up";
   }
+
   pikachu.style.top=posY+"px";
   pikachu.style.left=posX+"px";
   imgPikachu.setAttribute("src","assets/img/"+pokemon+direction+".png");
