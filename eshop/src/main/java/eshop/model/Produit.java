6f466dc40fb5a3c87e57878cf2bb7c2d5275a9a8
Jordan <Jordanabid@gmail.com>
2023-11-27 17:35:35 +0100

    Correction TP 4

--- eshop/src/main/java/eshop/model/Produit.java
+++ eshop/src/main/java/eshop/model/Produit.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -24,6 +25,7 @@ public class Produit {
 	private double prix; // double  prix
 	
 	@ManyToOne
+	@JoinColumn(name="vendeur", nullable = false)
 	private Fournisseur fournisseur;
 	
 	
