b653c47a78d1ed11fafe79d5b57d53a3c614b6ac
Eric SULTAN <e.sultan@ajc-ingenierie.fr>
2023-12-19 14:32:38 +0100

    todo-angular-v4

--- todo-angular/src/app/list/list.component.ts
+++ todo-angular/src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { List } from '../model';
 
 @Component({
   selector: 'app-list',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
+  lists: List[] = new Array<List>();
+  
+  listForm?: List = undefined;
+
+
+  constructor() {
+    this.lists.push(new List(4, "Tâche ménagère", 3));
+    this.lists.push(new List(5, "Liste de Noël", 3));
+    this.lists.push(new List(7, "Liste de mariage", 8));
+  }
+
+  list(): List[] {
+    return this.lists;
+  }
+
+  add() {
+    this.listForm = new List();
+  }
+
+  edit(id?: number) {
+    this.listForm = {...this.lists.find(list => list.id == id)??undefined};
+  }
+
+  save() {
+    
+  }
+
+  remove(id?: number) {
+    
+  }
+
+  cancel() {
+    this.listForm = undefined;
+  }
 
 }
--- todo-angular/src/app/todo/todo.component.ts
+++ todo-angular/src/app/todo/todo.component.ts
@@ -9,12 +9,16 @@ import { Todo } from '../model';
 export class TodoComponent {
   todos: Todo[] = new Array<Todo>();
 
-  todoForm: Todo | null = null;
+  todoForm?: Todo = undefined;
   
   constructor() {
     this.todos.push(new Todo(5, "Ménage", true, false, 4));
     this.todos.push(new Todo(50, "Vaisselle", false, true, 4));
     this.todos.push(new Todo(15, "Repassage", true, false, 4));
+  }
+
+  list(): Todo[] {
+    return this.todos;
   }
 
   add() {
@@ -24,13 +28,40 @@ export class TodoComponent {
   edit(id?: number) {
     for(let todo of this.todos) {
       if(todo.id == id) {
-        this.todoForm = todo;
+        this.todoForm = {...todo};
         break;    
       }
     }
   }
 
+  save() {
+    if(this.todoForm?.id) {
+      let position = this.todos.findIndex(todo => todo.id == this.todoForm?.id);
+
+      this.todos[position] = this.todoForm;
+    } else {
+      let max: number = 0;
+      this.todos.forEach(todo => {
+        if(todo.id && todo.id > max) {
+          max = todo.id;
+        }
+      });
+
+      this.todoForm!.id = max+1;
+
+      this.todos.push(this.todoForm!);
+    }
+
+    this.cancel();
+  }
+
+  remove(id?: number) {
+    let position = this.todos.findIndex(todo => todo.id == id);
+
+    this.todos.splice(position,1);
+  }
+
   cancel() {
-    this.todoForm = null;
+    this.todoForm = undefined;
   }
 }
