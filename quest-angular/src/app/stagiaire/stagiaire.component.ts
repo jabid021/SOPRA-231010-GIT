f57b0e61b9883b6e0c8ae867aed68df626e0f844
Eric SULTAN <e.sultan@ajc-ingenierie.fr>
2023-12-20 13:56:42 +0100

    quest-angular-v2

--- quest-angular/src/app/stagiaire/stagiaire.component.ts
+++ quest-angular/src/app/stagiaire/stagiaire.component.ts
@@ -30,14 +30,31 @@ export class StagiaireComponent {
   }
 
   edit(id?: number) {
-    
+    this.stagiaireService.findById(id).subscribe(resp => {
+      this.stagiaireForm = resp;
+    });
   }
 
   save() {
+    if(this.stagiaireForm) {
+      if(this.stagiaireForm.id) {
+        this.stagiaireService.update(this.stagiaireForm).subscribe(resp => {
+          this.load();
+        });
+      } else {
+        this.stagiaireService.create(this.stagiaireForm).subscribe(resp => {
+          this.load();
+        });
+      }
+    }
+
+    this.cancel();
   }
 
   remove(id?: number) {
-   
+    this.stagiaireService.delete(id).subscribe(() => {
+      this.load();
+    });
   }
 
   cancel() {
--- quest-angular/src/app/stagiaire/stagiaire.service.ts
+++ quest-angular/src/app/stagiaire/stagiaire.service.ts
@@ -13,4 +13,20 @@ export class StagiaireService {
   findAll(): Observable<Stagiaire[]> {
     return this.http.get<Stagiaire[]>("http://localhost:8080/api/stagiaire");
   }
+
+  findById(id?: number): Observable<Stagiaire> {
+    return this.http.get<Stagiaire>("http://localhost:8080/api/stagiaire/"+id);
+  }
+
+  create(stagiaire: Stagiaire): Observable<Stagiaire> {
+    return this.http.post<Stagiaire>("http://localhost:8080/api/stagiaire", stagiaire);
+  }
+
+  update(stagiaire: Stagiaire): Observable<Stagiaire> {
+    return this.http.put<Stagiaire>("http://localhost:8080/api/stagiaire/"+stagiaire.id, stagiaire);
+  }
+
+  delete(id?: number): Observable<void> {
+    return this.http.delete<void>("http://localhost:8080/api/stagiaire/"+id);
+  }
 }
